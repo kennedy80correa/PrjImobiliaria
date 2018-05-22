12f8cb9106ee543d3e6b6b7ada2f4b6be8b70ca5
Kennedy Correa <kennedy80correa@gmail.com>
2018-05-22 16:17:52 -0300

    Add Verifica()

--- Etapa-1_160080_160038.c
+++ Etapa-1_160080_160038.c
@@ -41,20 +41,24 @@ typedef struct adress{
 typedef struct info_loc{
     char sigla;
     int reg_loc;
-}inf_loc;
+}info_loc;
 
 void aloca_endereco(endereco **p, int tam);
 void aloca_info_casa(info_casa **p, int tam);
-void aloca_prop(prop **p, int tam);
+void aloca_prop(prop **p);
+void aloca_adress(adress **p, int tam);
+void aloca_info_loc(info_loc **p, int tam);
+int checa_arq();
 void cad_prop(prop *p, int num_reg);
-void consulta_prop();
+void consulta_prop(prop *p, int tam);
 void grava(prop *p);
-/*void cad_imovel();
-void consulta_imovel();
+void cad_imovel(adress *p);
+/*void consulta_imovel();
 */
 int main()
 {
 prop *p_prop=NULL;
+adress *p_imovel=NULL;
 int  cont=0,op;
 
 do{
@@ -64,17 +68,17 @@ do{
   fflush(stdin);
   switch(op)
     {
-    case 1: aloca_prop(&p_prop,cont+1);
+    case 1: aloca_prop(&p_prop);
             cad_prop(p_prop,cont);
             cont++;
     break;
 
     case 2: consulta_prop(p_prop,cont);
     break;
-/*
-    case 3: cad_imovel(p_prop,cont,&lista,&clista);
-    break;
 
+    case 3: cad_imovel(p_imovel);
+    break;
+/*
     case 4: consulta_imovel(p_prop,cont);
     break;
 */
@@ -95,9 +99,9 @@ void aloca_info_casa(info_casa **p, int tam)
 		exit(1);
 }
 
-void aloca_prop(prop **p, int tam)
+void aloca_prop(prop **p)
 {
-	if((*p=(prop*)realloc(*p,tam*sizeof(prop)))==NULL)
+	if((*p=(prop*)realloc(*p,sizeof(prop)))==NULL)
 	{
 		exit(1);
 	}
@@ -138,9 +142,15 @@ void cad_prop(prop *p, int i)
     printf("Email:");
     gets(p->end_prop.email);
     fflush(stdin);
-	printf("\nInformacao Casa:");
-	gets((p->casa+i)->status_casa);
-
+    if(i<5)
+    {
+        printf("\nInformacao Casa:");
+        gets((p->casa+i)->status_casa);
+    }
+	else
+    {
+            printf("Limite atingido(max. 5 Itens)");
+	}
 }
 
 void consulta_prop(prop *p, int tam){
@@ -151,9 +161,9 @@ void grava(prop *p)
 {
     FILE *fp=NULL;
 
-    if((fp=fopen("prop.bin","rb"))==NULL)
+    if((fp=fopen("prop.bin","ab"))==NULL)
     {
-        printf("\nErro na gravacao\n");
+        printf("\nErro ao Gravar!!!\n");
     }
     else
         {
@@ -164,10 +174,53 @@ void grava(prop *p)
 
 }
 
-/*
-void cad_imovel(prop *p, int tam){
 
+void cad_imovel(adress *p)
+{
+    fflush(stdin);
+    printf("Logradouro:");
+    gets(p->logradouro);
+    fflush(stdin);
+    printf("Bairro:");
+    gets(p->bairro);
+    fflush(stdin);
+    printf("Cidade:");
+    gets(p->cidade);
+    fflush(stdin);
+    printf("CEP:");
+    gets(p->CEP);
 }
+
+void aloca_adress(adress **p, int tam)
+{
+    if((*p=(adress*)realloc(*p,tam*sizeof(adress)))==NULL)
+    {
+        exit(1);
+    }
+}
+void aloca_info_loc(info_loc **p, int tam)
+{
+    if((*p=(info_loc*)realloc(*p,tam*sizeof(info_loc)))==NULL)
+    {
+        exit(1);
+    }
+}
+
+int checa_arq(){
+    FILE *f=NULL;
+    long int pos;
+
+    if((f=fopen("prop.bin", "rb"))==NULL){
+        printf("\nErro ao Gravar!!!\n");
+    }
+    else{
+        fseek(f,0,2);
+        pos=ftell(f)/sizeof(prop);
+        fclose(f);
+    }
+}
+
+/*
 void consulta_imovel(){
 
 }
